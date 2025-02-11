ba2096a914bb280bb9d319970f535b2a97013369
ijalandhika <ijalandhika@gmail.com>
2025-02-11 22:18:42 +0700

    add constants

---
+++ src/constants/role.ts
@@ -0,0 +1,29 @@
+export const ROLE_ADMIN = 'admin';
+
+export const ROLE_TEAM_LEADER = 'team_leader';
+
+export const ROLE_SALES = 'sales';
+
+export const SALES_MENU = {
+    customer: '/customers', 
+    task: '/tasks', 
+    pipeline: '/pipelines', 
+    report: '/reports'
+}
+
+export const TEAM_LEADER_MENU = {
+    customer: '/customers',
+    task: '/tasks',
+    team: '/teams',
+    pipeline: '/pipelines', 
+    report: '/reports'
+}
+
+export const ADMIN_MENU = {
+    customer: '/customers',
+    task: '/tasks',
+    team: '/teams',
+    pipeline: '/pipelines', 
+    report: '/reports'
+
+}
